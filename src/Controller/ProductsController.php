814e57fcbd502a8b89b991316c3fca191140f263
jmaldonado@itpatagonia.com <jorgeemilianom@gmail.com>
2023-12-07 17:58:29 -0300

    add base ProductController

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use AbstractBaseController;
+use App\Entity\Products;
+use App\Security\Validations;
+use Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController extends AbstractBaseController
+{
+
+    /**
+     * @Route("/products", name="get_products_all_business", methods={"GET", "OPTIONS"})
+     */
+    public function getAll(): JsonResponse
+    {
+        try {
+            if(Validations::checkToken()) return $this->responseTokenInvalid();
+
+            throw new Exception("Exception Test");
+            $allProducts = $this->entityManager->getRepository(Products::class)->findAll();
+            $allProducts = $allProducts ? $this->normalize($allProducts) : [];
+            return new JsonResponse([
+                'message' => 'Products list',
+                'products' => $allProducts
+            ], 200);
+        } catch (\Exception $e) {
+            return $this->ExceptionResponse($e);
+        }
+    }
+
+    /**
+     * @Route("/products", name="create_product", methods={"POST", "OPTIONS"})
+     */
+    public function createProduct(Request $request): JsonResponse
+    {
+        try {
+            if(Validations::checkToken()) return $this->responseTokenInvalid();
+
+            $parameters = $request->getContent();
+            $products_array = json_decode($parameters, true);
+
+            $allProducts = [];
+            foreach ($products_array as $productData) {
+                $product = new Products();
+                $product->setSku($productData['sku']);
+                $product->setProductName($productData['product_name']);
+                $product->setDescription($productData['description']);
+                $product->setCreatedAt(new \DateTime());
+                $product->setUpdatedAt(new \DateTime());
+                $this->entityManager->persist($product);
+                $this->entityManager->flush();
+                $allProducts[] = $this->normalize($product);
+            }
+
+            return new JsonResponse([
+                'message' => 'Added products',
+                'data' => $allProducts
+            ], 201);
+        } catch (\Exception $e) {
+            return $this->ExceptionResponse($e);
+        }
+    }
+
+    /**
+     * @Route("/products", name="update_product", methods={"PUT", "OPTIONS"})
+     */
+    public function updateProduct(Request $request): JsonResponse
+    {
+        try {
+            if(Validations::checkToken()) return $this->responseTokenInvalid();
+            
+            $parameters = $request->getContent();
+            $products_array = json_decode($parameters, true);
+
+            $allProducts = [];
+            foreach ($products_array as $key => $productData) {
+                $product = $this->entityManager
+                    ->getRepository(Products::class)
+                    ->findOneBy(['id' => $productData['id']]);
+                $product->setSku($productData['sku']);
+                $product->setProductName($productData['product_name']);
+                $product->setDescription($productData['description']);
+                $product->setUpdatedAt(new \DateTime());
+                $this->entityManager->persist($product);
+                $this->entityManager->flush();
+                $allProducts[] = $this->normalize($product);
+            }
+
+            return new JsonResponse([
+                'message' => 'Productos actualizados',
+                'data' => $allProducts
+            ], 201);
+        } catch (\Exception $e) {
+            return $this->ExceptionResponse($e);
+        }
+    }
+}
